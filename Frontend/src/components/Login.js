bc591f88fcc3d74ca581fc23a603e019072cd25e
ela2zofia <derrickli824@gmail.com>
2021-09-16 20:35:30 +0800

    Finish user page contact count and weather feature

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -40,32 +40,32 @@ function Login() {
     event.preventDefault();
     // console.log( username )
 
-    // dispatch( logIn( username ) );
-    // setSuccess( true );
-    // UserUtil.setUserWithExpiry( remember, username );
+    dispatch( logIn( username ) );
+    setSuccess( true );
+    UserUtil.setUserWithExpiry( remember, username );
 
 
-    if ( validate( username, password ) ) {
-      fetch( 'http://127.0.0.1:8000/login', {
-        //请求方法
-        method: 'POST',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify( { username: username, password: password } ),
-      }
-      ).then( response => {
-        return response.json();
-      } ).then( response => {
-        if ( response.isCorrect ) {
-          // Log in
-          dispatch( logIn( username ) );
-          setSuccess( true );
-          UserUtil.setUserWithExpiry( remember, username );
-        }
-        else {
-          alert( "Your username or password is incorrect" );
-        }
-      } );
-    }
+    // if ( validate( username, password ) ) {
+    //   fetch( 'http://127.0.0.1:8000/login', {
+    //     //请求方法
+    //     method: 'POST',
+    //     headers: { 'Content-type': 'application/json' },
+    //     body: JSON.stringify( { username: username, password: password } ),
+    //   }
+    //   ).then( response => {
+    //     return response.json();
+    //   } ).then( response => {
+    //     if ( response.isCorrect ) {
+    //       // Log in
+    //       dispatch( logIn( username ) );
+    //       setSuccess( true );
+    //       UserUtil.setUserWithExpiry( remember, username );
+    //     }
+    //     else {
+    //       alert( "Your username or password is incorrect" );
+    //     }
+    //   } );
+    // }
   }
 
 
